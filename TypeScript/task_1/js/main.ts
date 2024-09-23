f6139d7dea9df25c6431b834e3422ebc28dbe081
Thomas3300000 <8844@holbertonstudents.com>
2024-09-23 13:29:25 +0200

    Create function with interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,3 +6,19 @@ interface Teacher {
     location: string;
     [propName: string]: any;
 }
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+
+
+console.log(printTeacher("John", "Doe"));
